97a2eaa9bd0b10679a2813baf1b6461313db1cc8
Madhan Kumar B <madhandivine3@gmail.com>
2025-12-11 19:54:20 +0530

    first commit

--- src/test/java/stepDefinition/Hooks.java
+++ src/test/java/stepDefinition/Hooks.java
@@ -2,14 +2,14 @@ package stepDefinition;
 
 import Testcomponents.BaseTest;
 import io.cucumber.java.Before;
-
 import java.io.IOException;
 
 public class Hooks extends BaseTest
 {
+
     @Before
     public void setup() throws IOException
     {
-        loginPage = launchApplication();   // ✅ STORE RETURNED OBJECT
+        loginPage = launchApplication();  // IMPORTANT
     }
 }
